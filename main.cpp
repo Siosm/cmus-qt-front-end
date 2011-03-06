336e56f51cef92fcf596b1fca2fd725fee3bcb1a
Timothée Ravier <timothee.romain.ravier@gmail.com>
2011-03-06 19:02:50 +0100

    adding the beginning of system tray capabilities

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include <QtGui/QApplication>
+#include <QtGui/QSystemTrayIcon>
+#include <QtGui/QMessageBox>
+
 #include "mainwindow.h"
 #include "mainwindow2.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    if (!QSystemTrayIcon::isSystemTrayAvailable()) {
+        QMessageBox::critical(0, QObject::tr("Systray"),
+                              QObject::tr("I couldn't detect any system tray "
+                                          "on this system."));
+        return 1;
+    }
+    //QApplication::setQuitOnLastWindowClosed(false);
+
     MainWindow2 w;
     w.show();
 
